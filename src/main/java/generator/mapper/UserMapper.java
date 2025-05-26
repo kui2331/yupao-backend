3fe5801cb5946b940544dae9c534dbe2f2a5a602
kui2331 <157229009+kui2331@users.noreply.github.com>
2025-05-26 13:15:20 +0800

    后端初始化

---
+++ src/main/java/generator/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author kui23
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-05-26 11:19:37
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/generator/service/UserService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author kui23
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2025-05-26 11:19:37
+*/
+public interface UserService extends IService<User> {
+
+}
