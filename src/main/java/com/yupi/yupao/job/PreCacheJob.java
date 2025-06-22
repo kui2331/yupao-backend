5cf9a5ecae6ec9d182667c15abc82cf56756c9fc
kui2331 <157229009+kui2331@users.noreply.github.com>
2025-06-22 19:59:43 +0800

    feat(ui): 增加Redis缓存预热任务和测试用例 perf(ui): 对推荐用户接口增加Redis缓存处理

---
+++ src/main/java/com/yupi/yupao/job/PreCacheJob.java
@@ -0,0 +1,54 @@
+package com.yupi.yupao.job;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.yupao.model.domain.User;
+import com.yupi.yupao.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: shayu
+ * @date: 2022/12/11
+ * @ClassName: yupao-backend01
+ * @Description:        数据预热
+ */
+
+@Component
+@Slf4j
+public class PreCacheJob {
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    // 重点用户
+    private List<Long> mainUserList = Arrays.asList(1L);
+
+    // 每天执行，预热推荐用户
+    @Scheduled(cron = "0 12 1 * * *")   //自己设置时间测试
+    public void doCacheRecommendUser() {
+        //查数据库
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        Page<User> userPage = userService.page(new Page<>(1,20),queryWrapper);
+        String redisKey = String.format("shayu:user:recommend:%s",mainUserList);
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        //写缓存,30s过期
+        try {
+            valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
+        } catch (Exception e){
+            log.error("redis set key error",e);
+        }
+    }
+
+}
